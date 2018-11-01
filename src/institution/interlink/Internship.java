3d83dec74e153101532cfd493543814f79ac4590
MrKirat <44450483+MrKirat@users.noreply.github.com>
2018-11-02 01:36:25 +0200

    implementation of the class Internship

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -1,18 +1,32 @@
 package institution.interlink;
 
+import java.util.ArrayList;
+import java.util.List;
 import person.Student;
 
 public class Internship {
+    private String name;
+    private List <Student> students = new ArrayList <>(); // list of internship students
+    
     public Internship(String name) {
-        //TODO: Implementation is needed
+        this.name = name;
     }
 
-    public void setStudent(Student student) {
-        //TODO: Implementation is needed
+    public void setStudents(List <Student> students) { //adding students to the intenship
+        for(Student st: students){
+            if(!this.students.contains(st) && st.getKnowledge().getLevel() > 5){
+                this.students.add(st);
+            }
+        }
     }
 
-    public String getStudents() {
-        //TODO: Implementation is needed
-        return "Andrew Maslenko\nJulia Veselkina\n";
+    public List<Student> getStudents(){
+        return students;
+    }
+    
+    public void printStudents(){ //printing a list of student
+        for(Student st: students){
+            System.out.println(st.getName() + " with a level of knowledge " + st.getKnowledge().getLevel());
+        }
     }
 }
