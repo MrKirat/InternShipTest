eaead5637a466464c79c0fad03081081977a5460
Taras Viyatyk <tviyatyk@gmail.com>
2018-11-10 12:50:47 +0200

    Update Knowledge.java
    Change level type to double.

--- src/person/consciousness/Knowledge.java
+++ src/person/consciousness/Knowledge.java
@@ -1,20 +1,20 @@
 package person.consciousness;
 
 public class Knowledge {
-    private int level;
+    private double level;
     
     //in my program a student can acquire knowledge levels from 1 to 10
-    public Knowledge(int level) {
-        if (0 < level && level < 11){ //check the conditions of the level
+    public Knowledge(double level) {
+        if (0 < level && level <= 10){ //check the conditions of the level
             this.level = level;
         } else {
             //throw an error if the level of knowledge does not meet the conditions
-            throw new IllegalArgumentException("The level should be integer"
+            throw new IllegalArgumentException("The level should be double"
                     + " and get into range [1,10]: " + level);
         }
     }
 
-    public int getLevel(){
+    public double getLevel(){
         return level;
     }
 }   
